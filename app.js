bc33008c30e460f55f9d37ebc00217af31b2453b
sushi <sushmeet sunger>
2017-09-23 12:29:27 -0400

    some stuff

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-var express = require("express");
-var mongoose = require("mongoose");
-var bodyParser = require("body-parser");
+'use strict';
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
-app = express();
+const app = express();
 
 mongoose.connect("mongodb://localhost/restful_blog_app", {
     useMongoClient: true
@@ -18,28 +19,43 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Mongoose/Model config
-// var blogSchema = new mongoose.Schema({
-//   title: String,
-//   image: String,
-//   body: String,
-//   createdAt: { type: Date, default: Date.now },
-// });
+var blogSchema = new mongoose.Schema({
+  title: String,
+  image: String,
+  body: String,
+  createdAt: { type: Date, default: Date.now },
+});
 
 var blog = mongoose.model("blog", blogSchema);
 
-blog.create(
-  {
-    title: "Test Blog",
-    image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTE4bVLgh_5P8esGzn016ByN3IyjNSqmLalzVm2NXOq32FtMJy0Wg",
-    body: "This is a Blog Post"
-  },
-  (err, blogEntry) => {
-    console.log(blogEntry);
-  }
-);
+// blog.create(
+//   {
+//     title: "Test Blog",
+//     image:
+//       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTE4bVLgh_5P8esGzn016ByN3IyjNSqmLalzVm2NXOq32FtMJy0Wg",
+//     body: "This is a Blog Post"
+//   },
+//   (err, blogEntry) => {
+//     console.log(blogEntry);
+//   }
+// );
 
 // Resful Routes
+
+app.get('', (req, res) => {
+  res.redirect('/blogs');
+});
+
+app.get('/blogs',(req, res) => {
+  blog.find({}, (err, blogs) => {
+      if(err) {
+        console.log(err);
+      }
+      else {
+        res.render('index',{blogs: blogs});
+      }
+  })
+});
 
 app.listen(5000, () => {
   console.log("server is running on port 5000");
